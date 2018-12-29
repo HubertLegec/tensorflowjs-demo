44f79c2e1fa8c00541e54d1b58de407d23ada28b
Hubert Legęć <hubert.legec@gmail.com>
2018-12-29 01:27:13 +0100

    gitignore update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,16 +7,14 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo"/>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a className="App-link"
+             href="https://reactjs.org"
+             target="_blank"
+             rel="noopener noreferrer">
             Learn React
           </a>
         </header>
