ee5289f2f60d0c6fb636822bdb937f51bc5cd0ea
Hubert Legęć <hubert.legec@gmail.com>
2019-01-06 19:11:05 +0100

    MnistModelBuilder code refactoring

--- src/MnistModelBuilder.js
+++ src/MnistModelBuilder.js
@@ -5,41 +5,17 @@ export class MnistModelBuilder {
 
   static build() {
     const model = tf.sequential();
-    model.add(tf.layers.conv2d({
-      inputShape: [28, 28, 1],
-      kernelSize: 3,
-      filters: 16,
-      activation: 'relu'
-    }));
-    model.add(tf.layers.maxPooling2d({
-      poolSize: 2,
-      strides: 2
-    }));
-    model.add(tf.layers.conv2d({
-      kernelSize: 3,
-      filters: 32,
-      activation: 'relu'
-    }));
-    model.add(tf.layers.maxPooling2d({
-      poolSize: 2,
-      strides: 2
-    }));
-    model.add(tf.layers.conv2d({
-      kernelSize: 3,
-      filters: 32,
-      activation: 'relu'
-    }));
+    model.add(tf.layers.conv2d({inputShape: [28, 28, 1], kernelSize: 3, filters: 16, activation: 'relu'}));
+    model.add(tf.layers.maxPooling2d({poolSize: 2, strides: 2}));
+    model.add(tf.layers.conv2d({kernelSize: 3, filters: 32, activation: 'relu'}));
+    model.add(tf.layers.maxPooling2d({poolSize: 2, strides: 2}));
+    model.add(tf.layers.conv2d({kernelSize: 3, filters: 32, activation: 'relu'}));
     model.add(tf.layers.flatten());
     model.add(tf.layers.dense({units: 64, activation: 'relu'}));
-    model.add(tf.layers.dense({
-      units: 10,
-      activation: 'softmax'
-    }));
-    model.compile({
-      optimizer: MnistModelBuilder.optimizer,
-      loss: 'categoricalCrossentropy',
-      metrics: ['accuracy'],
-    });
+    model.add(tf.layers.dense({units: 10, activation: 'softmax'}));
+    model.compile(
+      {optimizer: MnistModelBuilder.optimizer, loss: 'categoricalCrossentropy', metrics: ['accuracy']}
+    );
     return model;
   }
 
